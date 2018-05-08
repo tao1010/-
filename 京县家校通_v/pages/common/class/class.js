f830042cc86154f855d45e7ac0512ebf4fd25ad2
TonnyTeng <1083683360@qq.com>
2018-05-08 17:40:12 +0800

    add 微信小程序

---
+++ 京县家校通_v/pages/common/class/class.js
@@ -0,0 +1,74 @@
+// pages/class/class.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banner: ["../../images/class/banner.png","../../images/class/banner.png"],
+    listArray: [
+      { "id": "101", "avatr": "https://tao1010.github.io/image/avatar.gif", "name": "邓涛1", "relation": "自己", "content": "开始编码:京县家校通小程序", "love": "1", "date": "2018-05-08 09:10:23" },
+      { "id": "102", "avatr": "https://tao1010.github.io/image/avatar.gif", "name": "邓涛2", "relation": "父亲", "content": "开始编码:京县家校通小程序", "love": "0", "date": "2018-05-08 10:10:23" },
+      { "id": "103", "avatr": "https://tao1010.github.io/image/avatar.gif", "name": "邓涛3", "relation": "母亲", "content": "开始编码:京县家校通小程序", "love": "0", "date": "2018-05-08 09:32:23" },
+      { "id": "104", "avatr": "https://tao1010.github.io/image/avatar.gif", "name": "邓涛4", "relation": "爷爷", "content": "开始编码:京县家校通小程序", "love": "1", "date": "2018-05-08 09:30:23" },
+      { "id": "105", "avatr": "https://tao1010.github.io/image/avatar.gif", "name": "邓涛5", "relation": "奶奶", "content": "开始编码:京县家校通小程序", "love": "0", "date": "2018-05-08 09:40:23" },
+      { "id": "106", "avatr": "https://tao1010.github.io/image/avatar.gif", "name": "邓涛6", "relation": "外公", "content": "开始编码:京县家校通小程序", "love": "1", "date": "2018-05-08 09:12:23" },
+      { "id": "107", "avatr": "https://tao1010.github.io/image/avatar.gif", "name": "邓涛7", "relation": "外婆", "content": "开始编码:京县家校通小程序", "love": "0", "date": "2018-05-08 09:12:23" },
+      ]
+  },
+  
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ 京县家校通_v/pages/components/classList/classList.js
@@ -0,0 +1,33 @@
+// pages/components/classList/classList.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+    avater: String,//头像url
+    name: String,//姓名
+    relation: String,//关系
+    content: String,//内容
+    date: String,//时间
+    isLove: Boolean,//是否点赞
+    isSelf: Boolean,//是否为自己- 显示删除按钮
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    //点赞
+    //评论
+    //详情
+    //删除
+  }
+})
