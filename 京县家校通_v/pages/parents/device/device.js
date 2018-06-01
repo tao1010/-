d125258b46991dd0b74f0a029651edf83452db21
TonnyTeng <1083683360@qq.com>
2018-06-01 16:06:34 +0800

    add

--- 京县家校通_v/pages/parents/device/device.js
+++ 京县家校通_v/pages/parents/device/device.js
@@ -124,6 +124,262 @@ Page({
         console.log(res)
       }
     })
+  },
+  wifi: function(){
+    wx.startWifi({
+      success: function(res){
+        console.log('success:' + res)
+      },
+      fail: function(res){
+        console.log('fail:' + res)
+      },
+      complete: function(res){
+        console.log('complete:' + res)
+      }
+    }),
+    wx.stopWifi({
+      success: function (res) {
+        console.log('success:' + res)
+      },
+      fail: function (res) {
+        console.log('fail:' + res)
+      },
+      complete: function (res) {
+        console.log('complete:' + res)
+      }
+    }),
+    wx.onGetWifiList(function(res){
+      console.log('onGetWifiList:' + res)
+    }),
+    wx.getWifiList({
+      success: function (res) {
+        console.log('success:' + res)
+      },
+      fail: function (res) {
+        console.log('fail:' + res)
+      },
+      complete: function (res) {
+        console.log('complete:' + res)
+      }
+    }),
+    wx.onWifiConnected(function(res){
+      console.log('onWifiConnected:' + res)
+    }),
+    wx.getConnectedWifi({
+      success: function (res) {
+        console.log('success:' + res)
+      },
+      fail: function (res) {
+        console.log('fail:' + res)
+      },
+      complete: function (res) {
+        console.log('complete:' + res)
+      }
+    }),
+    // wx.getHCEState({
+    //   success: function (res) {
+    //     console.log('success:' + res)
+    //   },
+    //   complete: function (res) {
+    //     console.log('complete:' + res)
+    //   }
+    // })
+    wx.connectWifi({
+      SSID: '',
+      BSSID: '',
+      success: function (res) {
+        console.log('connectWifi - success:' + res)
+      },
+      fail: function (res) {
+        console.log('connectWifi - fail:' + res)
+      },
+      complete: function (res) {
+        console.log('connectWifi - complete:' + res)
+      }
+    })
+  },
+  accelerometer: function(){
+
+      wx.stopAccelerometer({
+        success: function (res) {
+          console.log(res)
+        }
+      }),
+      wx.startAccelerometer({
+        success: function (res) {
+          console.log(res)
+        }
+      }),
+      wx.onAccelerometerChange(function(res){
+        console.log(res)
+      })
+  },
+  compass: function(){
+
+    wx.stopCompass({
+      success: function(res){
+        console.log(res)
+      }
+    }),
+
+    wx.startCompass({
+      success: function (res) {
+        console.log(res)
+      }
+    }),
+    
+    wx.onCompassChange(function (res) {
+      console.log(res)
+    })
+  },
+  ibeacon: function(){
+
+    wx.startBeaconDiscovery({
+      uuids: ["28287f6952aaa389e57a884342019129db67c0df"],
+      success: function(res){
+        console.log(res)
+      },
+      complete: function(res){
+        console.log(res)
+      }
+    }),
+    wx.stopBeaconDiscovery({
+      complete: function(res){
+        console.log(res)
+      }
+    }),
+    wx.getBeacons({
+      success: function (res) {
+        console.log('success:' + res)
+      },
+      fail: function (res) {
+        console.log('fail:' + res)
+      },
+      complete: function (res) {
+        console.log('complete:' + res)
+      }
+    }),
+    wx.onBeaconUpdate(function(res){
+      console.log(res)
+    }),
+    wx.onBeaconServiceChange(function(res){
+      console.log(res)
+    })
+  },
+  hce: function(){
+
+    wx.getHCEState({
+      success: function (res) {
+        console.log('success:' + res)
+      },
+      fail: function (res) {
+        console.log('fail:' + res)
+      },
+      complete: function (res) {
+        console.log('complete:' + res)
+      }
+    }),
+    wx.startHCE({
+      aid_list: [''],
+      success: function (res) {
+        console.log('success:' + res)
+      },
+      fail: function (res) {
+        console.log('fail:' + res)
+      },
+      complete: function (res) {
+        console.log('complete:' + res)
+      }
+    }),
+    wx.stopHCE({
+      success: function (res) {
+        console.log('success:' + res)
+      },
+      fail: function (res) {
+        console.log('fail:' + res)
+      },
+      complete: function (res) {
+        console.log('complete:' + res)
+      }
+    }),
+    wx.onHCEMessage(function(res){
+      console.log(res)
+    }),
+    wx.sendHCEMessage({
+      data: ['dasdadasfafd'],
+      success: function (res) {
+        console.log('success:' + res)
+      },
+      fail: function (res) {
+        console.log('fail:' + res)
+      },
+      complete: function (res) {
+        console.log('complete:' + res)
+      }
+    })
+  },
+  bluetooth:function(){
+    wx.openBluetoothAdapter({
+      success: function(res) {
+        console.log(res)
+      }
+    }),
+    wx.closeBluetoothAdapter({
+      success: function(res) {
+        console.log(res)
+      },
+    }),
+    wx.getBluetoothAdapterState({
+      success: function(res) {
+        console.log(res)
+      },
+    }),
+    wx.onBluetoothAdapterStateChange(function(res){
+      console.log(res)
+    }),
+    wx.startBluetoothDevicesDiscovery({
+      success: function(res) {
+        console.log(res)
+      },
+      complete: function(res){
+        console.log(res)
+      }
+    }),
+    wx.stopBluetoothDevicesDiscovery({
+      success: function (res) {
+        console.log(res)
+      },
+      complete: function (res) {
+        console.log(res)
+      }
+    }),
+    wx.getBluetoothDevices({
+      success: function (res) {
+        console.log(res)
+      },
+      complete: function (res) {
+        console.log(res)
+      }
+    }),
+    wx.onBluetoothDeviceFound(function(res){
+      console.log(res)
+    }),
+    wx.getConnectedBluetoothDevices({
+      services: [''],
+      success: function(res) {
+        console.log(res)
+      },
+      complete: function(res){
+        console.log(res)
+      }
+    }),
+    wx.createBLEConnection({
+      deviceId: 'xxx',
+      success: function(res) {
+        console.log(res)
+      },
+    })
+
   }
 })
 
